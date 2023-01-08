d964f52acb71473bb19c63050668e15ef58188ab
rafael <silva.rafa@hotmail.com>
2023-01-08 14:23:46 -0300

    consertado bug de acesso do banco de dados

--- pypro/settings.py
+++ pypro/settings.py
@@ -21,7 +21,6 @@ from sentry_sdk.integrations.django import DjangoIntegration
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -173,7 +172,7 @@ if AWS_ACCESS_KEY_ID:
 
     INSTALLED_APPS.append('s3_folder_storage')
     INSTALLED_APPS.append('storages')
-#ng tem isso
+
     DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
     
 
